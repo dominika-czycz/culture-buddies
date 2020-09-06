cf2bb0f5554a6ad48ac468eded364dccb73bbdc2
dominika-czycz <dominika.czycz@gmail.com>
2020-09-06 13:46:19 +0200

    creating application context test

---
+++ src/test/java/pl/coderslab/cultureBuddies/CultureBuddiesApplicationTest.java
@@ -0,0 +1,16 @@
+package pl.coderslab.cultureBuddies;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class CultureBuddiesApplicationTest {
+
+    @Test
+    public void contextLoad() {
+    }
+
+}
