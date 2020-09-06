db859991fde4bb46cfa4c87189b385103f680cdb
dominika-czycz <dominika.czycz@gmail.com>
2020-09-06 09:25:35 +0200

    home page with login form

---
+++ src/main/java/pl/coderslab/cultureBuddies/CultureBuddiesApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.cultureBuddies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureBuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureBuddiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/cultureBuddies/CultureBuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.cultureBuddies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureBuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
